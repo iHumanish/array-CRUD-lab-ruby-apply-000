4af29cbb1be85efa8f3b193583e09e2aac7647fd
kooky-calculator-3932 <ihumanish@gmail.com>
2019-06-10 02:22:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ def create_an_array = ['a', 'b', 'c', 'd']
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array.push['e']
+  puts add_element_to_end_of_array.push['e']
   
 end
 
